4758ab1bb2748c1c6f118af1543d21e1d2bd6678
Samuel Gruetter <saemi472@gmail.com>
2024-03-04 17:34:58 -0500

    smoke test: setting mtvec is really needed

--- run/setup.s
+++ run/setup.s
@@ -10,6 +10,9 @@ mystart:
         la a0, _shandlerinsts
         # low to bits of _shandlerinsts must be 0 so that mtvec_mode is direct
         # (as opposed to vectored)
+        # uncomment the next line to deliberately break it
+        # (but note that if we use IM, the trap handler is never called, so it still works)
+        # addi a0, a0, 42
         csrw mtvec, a0
         la a0, _escratch
         csrw mscratch, a0
